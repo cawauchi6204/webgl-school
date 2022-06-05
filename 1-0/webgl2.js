9e08ad1e8ca2d78d4ad5ede9de39d60bd7028709
cawauchi6204 <cawauchi6204@gmail.com>
2022-06-05 22:35:26 +0900

    refactor: 処理のスライド

--- 1-0/webgl2.js
+++ 1-0/webgl2.js
@@ -1,43 +1,29 @@
-//three.jsをロードする
 import * as THREE from '../lib/three.module.js'
 
 const TRANSPARENT = "0xFFFFFF"
 
 function week2() {
-
-  /**
-   * =============================================================
-   * Week2
-   * =============================================================
-   */
   const canvas = document.querySelector('#c-week2');
-  const renderer = new THREE.WebGLRenderer({ canvas }, { alpha: true });
-  renderer.setClearColor(TRANSPARENT, 0);//背景を透明にする
-
-  //hint:https://discourse.threejs.org/t/how-to-get-my-scene-background-to-be-transparent/11702
-
-  const scene = new THREE.Scene();
 
   const light = getLight();
-  scene.add(light)
-
   const propeller = getPropeller()
-  scene.add(propeller)
-
   const fanCoverCylinder = getFanCoverCylinder()
+
+  const scene = new THREE.Scene();
+  scene.add(light)
+  scene.add(propeller)
   scene.add(fanCoverCylinder);
 
   const camera = getCamera();
-  renderer.render(scene, camera)
 
-  //フラグ
+  const renderer = new THREE.WebGLRenderer({ canvas }, { alpha: true });
+  renderer.setClearColor(TRANSPARENT, 0);
+  renderer.render(scene, camera);
+
   let leftRotation = false;
   let rightRotation = false;
   let initRotation = false;
 
-  /**
-   * render関数
-   */
   function render() {
     propeller.rotation.z += 0.1
 
@@ -49,8 +35,16 @@ function week2() {
       fanCoverCylinder.rotation.z -= 0.005
     }
 
-    const AAA = fanCoverCylinder.rotation.z >= 1 && fanCoverCylinder.rotation.z >= 1.1
-    const BBB = fanCoverCylinder.rotation.z <= -1 && fanCoverCylinder.rotation.z <= -1.1
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+
+    const AAA = fanCoverCylinder.rotation.z >= 1 && fanCoverCylinder.rotation.z >= 1.1;
+    const BBB = fanCoverCylinder.rotation.z <= -1 && fanCoverCylinder.rotation.z <= -1.1;
     if (AAA) {
       leftRotation = false;
       rightRotation = true;
@@ -59,22 +53,11 @@ function week2() {
       leftRotation = true;
       rightRotation = false;
       initRotation = false;
-    } else {
-      if (!leftRotation) {
-        initRotation = true;
-      }
+    } else if (!leftRotation) {
+      initRotation = true;
     }
-
-    if (resizeRendererToDisplaySize(renderer)) {
-      const canvas = renderer.domElement;
-      camera.aspect = canvas.clientWidth / canvas.clientHeight
-      camera.updateProjectionMatrix()
-    }
-    renderer.render(scene, camera)
-    requestAnimationFrame(render)
   }
   requestAnimationFrame(render);
-
 }
 
 function getPropellerParts() {
@@ -88,15 +71,13 @@ function getPropellerParts() {
     PROPELLER_BOX_DEPTH
   )
 }
-
 function getFanCoverCylinder() {
   const fanCoverGeometry = new THREE.CylinderGeometry(8, 8, 4, 32);
   const fanCoverMaterial = new THREE.MeshPhongMaterial({ color: TRANSPARENT, wireframe: true });
   const fanCoverCylinder = new THREE.Mesh(fanCoverGeometry, fanCoverMaterial);
-  fanCoverCylinder.rotation.set(1.57, 0, 0)
+  fanCoverCylinder.rotation.set(1.57, 0, 0);
   return fanCoverCylinder;
 }
-
 function getCamera() {
   const FOV = 40;
   const ASPECT = 2;
@@ -107,14 +88,12 @@ function getCamera() {
   camera.position.y = 0;
   return camera
 }
-
 function getLight() {
   const INTENSITY = 1;
   const light = new THREE.DirectionalLight(TRANSPARENT, INTENSITY)
   light.position.set(-1, 2, 4)
   return light
 }
-
 function getPropeller() {
   const propellerMaterial = new THREE.MeshPhongMaterial({ TRANSPARENT, wireframe: true })
   const propeller = new THREE.Mesh(getPropellerParts(), propellerMaterial)
